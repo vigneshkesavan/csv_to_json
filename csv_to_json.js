a19c20cbf5d63f444dbdba950b15592ac5723423
vigneshkesavan <33860574+vigneshkesavan@users.noreply.github.com>
2019-11-15 22:31:46 +0530

    Add files via upload

---
+++ csv_to_json.js
@@ -0,0 +1,16 @@
+const csvjson = require('csvjson');
+const readFile = require('fs').readFile;
+var fs = require('fs');
+readFile('./sample.csv', 'utf-8', (err, fileContent) => {
+    if(err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+    }
+    const jsonObj = csvjson.toObject(fileContent);
+    console.log(jsonObj);
+	fs.writeFile ("myjsonfile.json", JSON.stringify(jsonObj,null,2), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    }
+	);
+});
